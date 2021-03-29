10d99233283a23815198fe5c920eadc37bcc4c20
paulnguyen <paul.nguyen@sjsu.edu>
2021-03-28 18:06:55 -0700

    Update server.go

--- labs/lab6/starbucks-api/src/starbucks/server.go
+++ labs/lab6/starbucks-api/src/starbucks/server.go
@@ -199,7 +199,7 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 // API Ping Handler
 func pingHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusOK, struct{ Test string }{"Starbucks API version 1.0 alive!"})
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"Starbucks API version 1.0.1 alive!"})
 	}
 }
 

21adb38e9cae812b7f95da5fad3122d2db53230c
paulnguyen <paul.nguyen@sjsu.edu>
2023-01-30 21:17:26 -0800

    Spring 2023

---
+++ demos/demo0/demo-intellij/src/test/java/com/example/demointellij/DemoIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demointellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIntellijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

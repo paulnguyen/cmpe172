289fe4d9d0d908016dfc5c534d5776c06f7bfd20
paulnguyen <paul.nguyen@sjsu.edu>
2023-02-06 21:15:57 -0800

    Spring 2023

---
+++ demos/demo0/demo-docker/src/test/java/com/example/demodocker/DemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
